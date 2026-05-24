989c44e5a9637d6fa19d3858ba2b5161dc76da3c
tash-IT <natashachuma1553@gmail.com>
2026-05-24 22:32:38 +0200

    Initial version of IT Asset Management

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+class Program
+{
+    static List<string> assets = new List<string>();
+    static void Main()
+    {
+        bool running = true;
+        while (running)
+        {
+            Console.WriteLine("===IT Asset Management===");
+            Console.WriteLine("1. Add Asset");
+            Console.WriteLine("2. View Asset");
+            Console.WriteLine("3. Exit");
+            Console.WriteLine("Choose option");
+
+            string choice = Console.ReadLine();
+            switch(choice)
+            {
+                case "1":
+                AddAsset();
+                break;
+
+                case "2":
+                ViewAssets();
+                break;
+
+                case "3":
+                running = false;
+                Console.WriteLine("Exiting Program....");
+                break;
+
+                default:
+                 Console.WriteLine("Invalid Option!");
+                 break;
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void AddAsset()
+    {
+        Console.Write("Enter Asset name: ");
+        string assetName = Console.ReadLine();
+
+        assets.Add(assetName);
+
+        Console.WriteLine($"Asset '{assetName}' added succesfully.");
+    }
+    static void ViewAssets()
+    {
+        Console.WriteLine("===Asset List===");
+
+        foreach(string asset in assets)
+        {
+            Console.WriteLine(asset);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# IT Asset Management System
+ A simple C# console application for managing IT assets within an organization.
+
+ ## Features
+ -Add assets
+ -View Assets
+ -Console-based menu system
+
+ ## Technologies used
+ -C#
+ -.NET Console Application
+
+## Purpose
+This project was created tonimprove practical software development skills and understanding of IT operational systems.
